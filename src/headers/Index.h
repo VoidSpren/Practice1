9736d2419f686ffa9e7633c2c18cf037a3ba4eaa
VoidSpren <juancho.chiquiza12@gmail.com>
2023-04-12 18:57:42 -0500

    added multiple parts of the hashtable

--- src/headers/Index.h
+++ src/headers/Index.h
@@ -12,17 +12,38 @@ struct INDEXNODE
     Index index;
 }typedef IndexNode;
 
-IndexNode *insertIndex(IndexNode *head, Index val){
-    IndexNode *node = (IndexNode*)malloc(sizeof(IndexNode));
-    node->next = NULL;
-    node->index = val;
+IndexNode *insertIndex(IndexNode *node, Index val){
+    IndexNode *newNode = (IndexNode*)malloc(sizeof(IndexNode));
+    newNode->next = NULL;
+    newNode->index = val;
 
-    if(head == NULL) return node;
+    if(node == NULL) return newNode;
 
-    IndexNode *child = head->next;
-    head->next = node;
-    node->next = child;
+    IndexNode *child = node->next;
+    node->next = newNode;
+    newNode->next = child;
 
-    return node;
+    return newNode;
 }
 
+IndexNode *deleteNode(IndexNode *parent, IndexNode *node){
+    if(node == NULL) return parent;
+
+    IndexNode *child = node->next;
+    
+    free(node);
+    if(parent == NULL) return child;
+
+    parent->next = child;
+    return parent;
+}
+
+int deleteAll(IndexNode *head){
+    int i = 0;
+    
+    for(;head != NULL; i++){
+        head = deleteNode(NULL, head);
+    }
+
+    return i;
+}
---
+++ src/server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stat.h>
+#include <fcntl.h>
+#include <mman.h>
+#include <semaphore.h>
+
+#include <headers/TravelInfo.h>
+#include <headers/TravInfFID.h>
+#include <headers/Index.h>
+
+int main(int argc, char* argv[]){
+    
+}
